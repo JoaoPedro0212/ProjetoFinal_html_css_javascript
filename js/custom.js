d4b41cac948c1ae914b2d2880f7bcad8c860b06b
JoaoPedro0212 <pinheiro.joao11@aluno.ifce.edu.br>
2025-02-08 18:40:02 -0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -12,9 +12,40 @@ const cesta = {
     x: telaJogo.width / 2 - 40,
     y: telaJogo.height - 80,
     largura: 200,
-    altura: 40,
+    altura: 90,
     velocidade: 20
 };
+
+const imagensFrutas = {
+  red: new Image(),
+  green: new Image(),
+  yellow: new Image(),
+  purple: new Image()
+};
+
+imagensFrutas.red.src = "../img/banana.png";
+imagensFrutas.green.src = "../img/laranja.png";
+imagensFrutas.yellow.src = "../img/melancia.png";
+imagensFrutas.purple.src = "../img/morango.png";
+
+const imagemCesta = new Image();
+imagemCesta.src = "../img/cesta-de-piquenique.png";
+
+const musicaFundo = new Audio("../sound/musica-fundo.mp3");
+musicaFundo.volume = 0.5;
+musicaFundo.loop = true;
+musicaFundo.play();
+
+
+const somColetado = new Audio("../sound/coleta.mp3");
+const somPerdido = new Audio("../sound/perdido.mp3");
+somColetado.volume = 0.7;
+somPerdido.volume = 0.7;
+
+function tocarSom(som) {
+    som.currentTime = 0;
+    som.play();
+  }
 
 function redimensionarCanvas() {
     telaJogo.width = window.innerWidth;
@@ -22,6 +53,7 @@ function redimensionarCanvas() {
     cesta.x = telaJogo.width / 2 - 40;
     cesta.y = telaJogo.height - 80;
 }
+
 window.addEventListener('resize', () => {
     const frutasBackup = frutas.slice();
     redimensionarCanvas();
@@ -52,11 +84,13 @@ function atualizarFrutas() {
         if (f.y + 20 >= cesta.y && f.x > cesta.x && f.x < cesta.x + cesta.largura) {
             pontos += 10;
             document.getElementById('pontos').innerText = `⭐ Acertos: ${pontos}`;
+            tocarSom(somColetado);
             return false;
         }
         if (f.y >= telaJogo.height) {
             perdidos++;
             document.getElementById('perdidos').innerText = `❌ Perdidos: ${perdidos}`;
+            tocarSom(somPerdido);
             return false;
         }
         return true;
@@ -67,18 +101,12 @@ function atualizarFrutas() {
 
 function desenharFrutas() {
     frutas.forEach(f => {
-        ctx.beginPath();
-        ctx.fillStyle = f.cor;  
-        ctx.arc(f.x, f.y, 20, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.closePath();
+        ctx.drawImage(imagensFrutas[f.cor], f.x - 20, f.y - 20, 40, 40);
     });
 }
 
-
 function desenharCesta() {
-    ctx.fillStyle = "brown";
-    ctx.fillRect(cesta.x, cesta.y, cesta.largura, cesta.altura);
+    ctx.drawImage(imagemCesta, cesta.x, cesta.y,cesta.largura, cesta.altura);
 }
 
 function loopJogo() {
@@ -91,6 +119,7 @@ function loopJogo() {
 }
 
 function gameOver() {
+    musicaFundo.pause();
     localStorage.setItem("pontuacao", pontos);
     window.location.href = "../html/gameover.html";
 }
